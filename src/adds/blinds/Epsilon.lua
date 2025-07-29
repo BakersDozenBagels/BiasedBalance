e1fe82a38ea3d273cbb79f2eaf11bbe25f409474
Mills-44 <mylesbrodsky44@gmail.com>
2025-07-28 20:45:00 -0500

    Add files via upload

--- src/adds/blinds/Epsilon.lua
+++ src/adds/blinds/Epsilon.lua
@@ -4,4 +4,30 @@ SMODS.Blind {
     pos = {
         x = 0,
         y = 0
-    }
+    },
+    discovered = false,
+    unlocked = true,
+    order = 1,
+    boss_colour = HEX("870f52"),
+    boss = {
+		min = 4,
+	},
+   calculate = function(self, blind, context)
+        if not blind.disabled then
+            if context.debuff_card and context.debuff_card.area ~= G.jokers then
+                return {
+                    debuff = true
+                }
+            end
+            if context.selling_card and context.card.ability.set == 'Joker' then
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'immediate',
+                    func = function()
+                        blind:disable()
+                        return true
+                    end
+                }))
+            end
+        end
+    end
+}
