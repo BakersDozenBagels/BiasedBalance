fe01b29ac972792a38d3d00e6652cf2adf8deb19
Mills-44 <mylesbrodsky44@gmail.com>
2025-07-29 15:42:25 -0500

    Add files via upload

--- lib/utils.lua
+++ lib/utils.lua
@@ -28,68 +28,6 @@ function BiasedBalance.is_even(card)
     return (BiasedBalance.is_numbered(card) and card.base.nominal%2 == 0) or card:get_id() == 14
 end
 
---Banned Jokers
-local function ban()
-    local banned_jokers = {
-        { "8_ball", "smiley",  "superposition", "walkie_talkie" },
-        { "ceremonial", "loyalty_card", "dusk", "seeing_double", "matador", "acrobat" },
-        { "campfire", "hit_the_road" }
-    }
-
-    local function ban_one(key, rarity)
-        G.P_CENTERS[key] = nil
-        SMODS.Centers[key] = nil
-        for _, pool in pairs(G.P_CENTER_POOLS) do
-            local ix = 1
-            while ix < #pool do
-                if pool[ix].key == key then
-                    table.remove(pool, ix)
-                else
-                    ix = ix + 1
-                end
-            end
-        end
-        local ix = 1
-        while ix < #SMODS.Center.obj_buffer do
-            if SMODS.Center.obj_buffer[ix] == key then
-                table.remove(SMODS.Center.obj_buffer, ix)
-            else
-                ix = ix + 1
-            end
-        end
-        if rarity then
-            ix = 1
-            while ix < #G.P_JOKER_RARITY_POOLS[rarity] do
-                if G.P_JOKER_RARITY_POOLS[rarity][ix].key == key then
-                    table.remove(G.P_JOKER_RARITY_POOLS[rarity], ix)
-                else
-                    ix = ix + 1
-                end
-            end
-        end
-    end
-
-    for i, row in pairs(banned_jokers) do
-        for _, v in pairs(row) do
-            ban_one("j_" .. v, i)
-        end
-    end
-
-    -- Banned Consumables
-    ban_one("c_grim")
-    
-end
-
-local raw_Game_init_item_prototypes = Game.init_item_prototypes
-
-function Game:init_item_prototypes()
-    raw_Game_init_item_prototypes(self)
-    ban()
-end
-
-ban()
---#endregion
-
 function BiasedBalance.reroll_scale()
     return G.GAME.modifiers.reroll_scale or 1
 end
