af9f616d0c033906ccda2fbf331c927aa2ca4260
InfinityPlus05 <piercove2005@gmail.com>
2026-01-21 22:20:57 -0500

    fixed lingering resentment

--- src/adds/jokers/Lingering_Resentment.lua
+++ src/adds/jokers/Lingering_Resentment.lua
@@ -25,11 +25,11 @@ SMODS.Joker {
     end,
     add_to_deck = function(self, card, from_debuff)
         G.GAME.round_resets.discards = G.GAME.round_resets.discards - card.ability.extra.d_size
-        ease_discard(card.ability.extra.d_size)
+        ease_discard(-card.ability.extra.d_size)
     end,
     remove_from_deck = function(self, card, from_debuff)
         G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.d_size
-        ease_discard(-card.ability.extra.d_size)
+        ease_discard(card.ability.extra.d_size)
     end,
     calculate = function(self, card, context)
         if context.joker_main then 
