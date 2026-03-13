6474a8e44739cdf20dbbafdd7839d60558797aea
InfinityPlus05 <piercove2005@gmail.com>
2026-03-13 17:13:15 -0400

    fixed engineer deck

--- src/adds/backs/Engineer.lua
+++ src/adds/backs/Engineer.lua
@@ -11,25 +11,24 @@ SMODS.Back {
     end,
     apply = function(self)
 		G.E_MANAGER:add_event(Event({
-			func = function()
-				if G.jokers then
-                    if pseudorandom('RareEngineer') <= 0.5 then
-                        SMODS.add_card {
-                                set = 'Utility',
-                                rarity = 'Rare',
-                                key_append = 'Mystery_Box'
-                            }
-                    else
-                        SMODS.add_card {
-                                set = 'Utility',
-                                rarity = 'Uncommon',
-                                key_append = 'Mystery_Box'
-                            }
+        func = function() 
+                local jokers = {}
+                for k, v in pairs(get_current_pool('Utility')) do
+                    if G.P_CENTERS[v] then
+                        if pseudorandom('rare') <= 0.5 then
+                            if G.P_CENTERS[v].set == 'Joker' and Card.is_rarity({ability = {set = 'Joker'}, config = {center = G.P_CENTERS[v]}}, 'Uncommon') then
+                                table.insert(jokers, G.P_CENTERS[v].key)
+                            end
+                        else
+                            if G.P_CENTERS[v].set == 'Joker' and Card.is_rarity({ability = {set = 'Joker'}, config = {center = G.P_CENTERS[v]}}, 'Rare') then
+                                table.insert(jokers, G.P_CENTERS[v].key)
+                            end
+                        end
                     end
-                    
-				end
+                end
+                local key = pseudorandom_element(jokers, 'engineer')
+                SMODS.add_card({key = key})
                 return true
-			end,
-		}))
+        end})) 
 	end,
 }
