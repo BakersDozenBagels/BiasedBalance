0ee6cb29560aceb566bdb6fbe50c341a42b56284
InfinityPlus05 <piercove2005@gmail.com>
2026-05-28 22:50:09 -0400

    diamond is now meaner

--- src/adds/blinds/Beta.lua
+++ src/adds/blinds/Beta.lua
@@ -11,13 +11,12 @@ SMODS.Blind {
     boss_colour = HEX("83d1db"),
     calculate = function(self, blind, context)
         if context.before and not G.GAME.blind.disabled then
-            G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2, func = function()
             for i = 1, #G.hand.cards do
                 if not G.hand.cards[i].debuffed then
-                    G.E_MANAGER:add_event(Event({func = function() SMODS.debuff_card(G.hand.cards[i], true, 'diamond') G.hand.cards[i]:juice_up(); return true end })) 
+                    SMODS.debuff_card(G.hand.cards[i], true, 'diamond') 
+                    G.hand.cards[i]:juice_up()
                 end
             end
-            return true end })) 
         end
     end,
     disable = function(self)
