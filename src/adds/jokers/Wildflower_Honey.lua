e347b3c3bad6ad08e7202ec38f282497cdef42a1
InfinityPlus05 <piercove2005@gmail.com>
2026-06-05 17:50:41 -0400

    attempted to have wildflower honey do payouts on screen

--- src/adds/jokers/Wildflower_Honey.lua
+++ src/adds/jokers/Wildflower_Honey.lua
@@ -26,11 +26,13 @@ SMODS.Joker {
     end,
     calculate = function(self, card, context)
         if context.skip_blind then
-            ease_dollars(card.ability.extra.money)
+            SMODS.calculate_effect {
+                    dollars = card.ability.extra.money,
+                    card = card,
+                }
             G.E_MANAGER:add_event(Event({
                     func = function()
                         card.ability.extra.money = math.max(card.ability.extra.money - card.ability.extra.money_mod,0)
-                        card:juice_up()
                         return true
                     end
                 }))
@@ -63,7 +65,37 @@ SMODS.Joker {
                 }
             end
         end
-        if context.end_of_round and context.game_over == false and context.main_eval then 
+        if context.starting_shop then
+            if card.ability.extra.money <= 0 then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        -- This replicates the food destruction effect
+                        -- If you want a simpler way to destroy Jokers, you can do card:start_dissolve() for a dissolving animation
+                        -- or just card:remove() for no animation
+                        play_sound('tarot1')
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.3,
+                            blockable = false,
+                            func = function()
+                                card:remove()
+                                return true
+                            end
+                        }))
+                        return true
+                    end
+                }))
+                return {
+                    message = localize('k_drank_ex'),
+                    colour = G.C.FILTER
+                }
+            end
+        end
+        if context.end_of_round and context.game_over == false and context.main_eval then --[[
             G.E_MANAGER:add_event(Event({
                     func = function()
                         ease_dollars(card.ability.extra.money)
@@ -71,35 +103,11 @@ SMODS.Joker {
                         card:juice_up()
                         return true
                     end
-                }))
-            if card.ability.extra.money <= 0 then
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        -- This replicates the food destruction effect
-                        -- If you want a simpler way to destroy Jokers, you can do card:start_dissolve() for a dissolving animation
-                        -- or just card:remove() for no animation
-                        play_sound('tarot1')
-                        card.T.r = -0.2
-                        card:juice_up(0.3, 0.4)
-                        card.states.drag.is = true
-                        card.children.center.pinch.x = true
-                        G.E_MANAGER:add_event(Event({
-                            trigger = 'after',
-                            delay = 0.3,
-                            blockable = false,
-                            func = function()
-                                card:remove()
-                                return true
-                            end
-                        }))
-                        return true
-                    end
-                }))
-                return {
-                    message = localize('k_drank_ex'),
-                    colour = G.C.FILTER
-                }
-            end
+                }))]]
+            card.ability.extra.money = math.max(card.ability.extra.money - card.ability.extra.money_mod,0)
         end
+    end,
+    calc_dollar_bonus = function(self, card)
+        return card.ability.extra.money + 1
     end
 }
